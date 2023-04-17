8089adc892e421431973245e59d353f7b6f39913
Huong Yen <83935090+britotonakai@users.noreply.github.com>
2023-04-17 17:12:00 +0700

    Share + Create Note

--- CK/app/src/main/java/com/example/homescreen/NoteScreen.java
+++ CK/app/src/main/java/com/example/homescreen/NoteScreen.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -14,19 +15,50 @@ import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toolbar;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class NoteScreen extends Fragment {
     Button btnBack;
     ImageButton btnPen, btnTable, btnImage, btnBulletList, btnText, btnCheckList, btnShare;
+    EditText editTextNote;
+    TextView textViewCurrentDay;
     MainScreen mainScreen;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_new_note, container, false);
         btnBack = view.findViewById(R.id.btnBack);
+        btnShare = (ImageButton) view.findViewById(R.id.btnShare);
+        editTextNote = view.findViewById(R.id.editTextNote);
+        textViewCurrentDay = view.findViewById(R.id.textViewCurrentDay);
         mainScreen = (MainScreen)getActivity();
+
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat day_month_year_time = new SimpleDateFormat("HH:mm aaa, dd LLLL, yyyy");
+        String dateTime = day_month_year_time.format(calendar.getTime()).toString();
+        textViewCurrentDay.setText(dateTime);
+
+        btnShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent shareIntent = new Intent();
+                shareIntent.setAction(Intent.ACTION_SEND);
+                shareIntent.putExtra(Intent.EXTRA_TEXT, editTextNote.getText().toString());
+                shareIntent.setType("text/plain");
+
+                if(shareIntent.resolveActivity(getActivity().getPackageManager()) != null){
+                    startActivity(shareIntent);
+                }
+
+            }
+        });
 
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
